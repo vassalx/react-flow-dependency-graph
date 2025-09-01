0394a78083f1d515882f4a179c920e240225c4ab
Oleksandr <Oleksandr.Vasylenko@ibvcloud.eu>
2025-09-01 13:40:54 +0300

    fixed build

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,25 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    target: "es2015", // ensure broad browser support
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        format: "iife", // single self-executing bundle
+        entryFileNames: "index.js",
+        chunkFileNames: "index.js", // force one file
+        assetFileNames: (assetInfo) => {
+          if (
+            assetInfo.name &&
+            assetInfo.name.indexOf(".css", assetInfo.name.length - 4) !== -1
+          ) {
+            return "index.css";
+          }
+          return assetInfo.name ?? "[name][extname]";
+        },
+      },
+    },
+  },
   base: "/react-flow-chart-poc/",
 });
