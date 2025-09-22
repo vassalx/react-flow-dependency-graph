187ef0354e6886336a47724ee5689ae5567b8253
Oleksandr <Oleksandr.Vasylenko@ibvcloud.eu>
2025-09-22 16:07:58 +0300

    updated Controls

--- src/components/LayoutFlow.tsx
+++ src/components/LayoutFlow.tsx
@@ -14,6 +14,8 @@ import "@xyflow/react/dist/style.css";
 
 import { useEffect, useState } from "react";
 import {
+  CustomEdgeProps,
+  CustomNodeProps,
   DiagramData,
   edgeTypes,
   nodeTypes,
@@ -24,6 +26,8 @@ import getElkLayout, { ElkDirectionType } from "../common/getElkLayout";
 import PositioningTools from "./PositionTools";
 import DiagramLegend from "./DiagramLegend";
 import LoadingOverlay from "./LoadingOverlay";
+import normalizeEdges from "../common/normalizeEdges";
+import normalizeNodes from "../common/normalizeNodes";
 
 const localStorageDirKey = "dir_";
 const localStorageDraggableKey = "draggable"
@@ -38,7 +42,7 @@ const LayoutFlow = () => {
   const [id, setId] = useState<string>("");
   const [items, setItems] = useState<{ [key: string]: string }>({});
   const [draggable, setDraggable] = useState<boolean>(
-    localStorage.getItem(localStorageDraggableKey) === "true"
+    !(localStorage.getItem(localStorageDraggableKey) === "false")
   );
 
   const updateELKLayout = async (oldNodes: Node[], oldEdges: Edge[]) => {
@@ -122,8 +126,126 @@ const LayoutFlow = () => {
 
   return (
     <ReactFlow
-      defaultNodes={[]}
-      defaultEdges={[]}
+      defaultNodes={normalizeNodes([
+        {
+          id: "1",
+          data: {
+            label: "Microsoft Corporation",
+            link: "https://www.microsoft.com/uk-ua/",
+          },
+        },
+        { id: "2", data: { label: "Satya Nadella - CEO", type: "person" } },
+
+        {
+          id: "3",
+          data: { label: "Azure (Cloud Services)" },
+        },
+        {
+          id: "3a",
+          data: { label: "Scott Guthrie - EVP, Cloud & AI", type: "person" },
+        },
+
+        { id: "4", data: { label: "Windows & Devices" } },
+        {
+          id: "4a",
+          data: {
+            label: "Panos Panay - EVP, Windows & Devices",
+            type: "person",
+          },
+        },
+
+        {
+          id: "5",
+          data: { label: "Office & Productivity" },
+        },
+        {
+          id: "5a",
+          data: { label: "Rajesh Jha - EVP, Office & Teams", type: "person" },
+        },
+
+        {
+          id: "6",
+          data: { label: "Gaming (Xbox, Activision)" },
+        },
+        {
+          id: "6a",
+          data: { label: "Phil Spencer - CEO, Gaming", type: "person" },
+        },
+
+        {
+          id: "7",
+          data: { label: "LinkedIn", link: "https://www.linkedin.com/feed/" },
+        },
+        {
+          id: "7a",
+          data: { label: "Ryan Roslansky - CEO, LinkedIn", type: "person" },
+        },
+
+        { id: "8", data: { label: "GitHub", link: "https://github.com/" } },
+        {
+          id: "8a",
+          data: { label: "Thomas Dohmke - CEO, GitHub", type: "person" },
+        },
+      ] as CustomNodeProps[])}
+      defaultEdges={normalizeEdges([
+        {
+          id: "e1",
+          source: "2",
+          target: "1",
+          label: "CEO of",
+          data: { lineType: "dashed" },
+        },
+
+        {
+          id: "e2",
+          source: "3a",
+          target: "3",
+          label: "Leads",
+          data: { lineType: "dashed" },
+        },
+        {
+          id: "e3",
+          source: "4a",
+          target: "4",
+          label: "Leads",
+          data: { lineType: "dashed" },
+        },
+        {
+          id: "e4",
+          source: "5a",
+          target: "5",
+          label: "Leads",
+          data: { lineType: "dashed" },
+        },
+        {
+          id: "e5",
+          source: "6a",
+          target: "6",
+          label: "Leads",
+          data: { lineType: "dashed" },
+        },
+        {
+          id: "e6",
+          source: "7a",
+          target: "7",
+          label: "CEO of",
+          data: { lineType: "dashed" },
+        },
+        {
+          id: "e7",
+          source: "8a",
+          target: "8",
+          label: "CEO of",
+          data: { lineType: "dashed" },
+        },
+
+        { id: "e8", source: "1", target: "3" },
+        { id: "e9", source: "1", target: "4" },
+        { id: "e10", source: "1", target: "5" },
+        { id: "e11", source: "1", target: "6" },
+        { id: "e12", source: "1", target: "7" },
+        { id: "e13", source: "1", target: "8" },
+      ] as CustomEdgeProps[])}
       fitView
       nodeTypes={nodeTypes}
       edgeTypes={edgeTypes}
